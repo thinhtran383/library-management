54d98d792b27d4f155fce519087f08c41d0f396e
ThinhTran <thinhtran3803@gmail.com>
2024-08-19 21:42:17 +0700

    test 2

--- src/main/java/com/example/library/common/TestFlow.java
+++ src/main/java/com/example/library/common/TestFlow.java
@@ -3,7 +3,15 @@ package com.example.library.common;
 public class TestFlow {
     private static volatile TestFlow instance;
 
-    public void Test(){
+    public void Test() {
         System.out.println("Test");
+    }
+
+    private TestFlow() {
+        // private constructor để ngăn chặn tạo instance từ bên ngoài
+    }
+
+    public void Thing()      {
+        System.out.println("Thing");
     }
 }
