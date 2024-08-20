cdc626a60dc16e39fade1ff27504407adea4a7ec
ThinhTran <thinhtran3803@gmail.com>
2024-08-20 08:34:01 +0700

    delete

--- src/main/java/com/example/library/common/TestFlow.java
+++
@@ -1,9 +0,0 @@
-package com.example.library.common;
-
-public class TestFlow {
-    private static volatile TestFlow instance;
-
-    public void Test(){
-        System.out.println("Test");
-    }
-}
